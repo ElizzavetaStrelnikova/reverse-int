fe45b3cc99d02b2897c3b2f3e9e2df42dfe203b8
ElizzavetaStrelnikova <elizaveta.strelnikova357@gmail.com>
2023-01-10 15:06:36 +0300

    init: write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let result='';
+  n = n.toString(10);
+
+  if (n[0] === '-') {
+    for (let i = n.length - 1; i >= 1; i --) {
+     result += n[i];
+    } 
+  } else 
+    for (let i = n.length - 1; i >= 0; i --) {
+     result += n[i];
+    } 
+  return result;
 }
